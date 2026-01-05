5dbc89768c4cfa10830b507e0fec7c6cadae9e4c
HEMANTH KUMAR B <0040hemanth@gmail.com>
2026-01-05 13:15:12 +0530

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,50 @@
+public class Account {
+
+    private int accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    // Constructor
+    public Account(int accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    // Deposit money
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful.");
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    // Withdraw money
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful.");
+        } else {
+            System.out.println("Insufficient balance or invalid amount.");
+        }
+    }
+
+    // Get current balance
+    public double getBalance() {
+        return balance;
+    }
+
+    // Get account number
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    // Display account details
+    public void displayAccountDetails() {
+        System.out.println("Account Number : " + accountNumber);
+        System.out.println("Account Holder : " + accountHolderName);
+        System.out.println("Balance        : " + balance);
+    }
+}
